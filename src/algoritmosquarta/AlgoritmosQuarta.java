e5d9d4ea16459a73d7025dd2f46743ff0f8b7060
Viktor Dominic Varantes Ramos <182110029@senacinformatica.br>
2022-06-29 15:14:33 -0300

    primeiro commit

---
+++ src/algoritmosquarta/AlgoritmosQuarta.java
@@ -0,0 +1,16 @@
+package algoritmosquarta;
+
+/**
+ *
+ * @author Viktor Dominic
+ */
+public class AlgoritmosQuarta {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("HELLO WORLD");
+    }
+    
+}
